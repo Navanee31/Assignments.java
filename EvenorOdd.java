9dc1122303e84cf0254a3a497031e07c036a02c3
Navaneeth <navaneethmortha123@gmail.com>
2026-04-05 11:09:00 +0530

    Add files via upload

---
+++ EvenorOdd.java
@@ -0,0 +1,18 @@
+package javalearning;
+
+import java.util.Scanner;
+
+public class EvenorOdd {
+
+	public static void main(String[] args) {
+		Scanner scanner=new Scanner(System.in);
+		System.out.println("Enter the number :");
+		int number=scanner.nextInt();
+		if(number%2==0) {
+			System.out.println(+number +"is an even number");
+		}else {
+			System.out.println(+number +" is an odd number");
+		}
+	}
+
+}
