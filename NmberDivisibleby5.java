f0a511077448a60efe7f903d7f9cdc3017ca189b
Navaneeth <navaneethmortha123@gmail.com>
2026-04-05 11:57:53 +0530

    Add files via upload

---
+++ NmberDivisibleby5.java
@@ -0,0 +1,19 @@
+package javalearning;
+
+import java.util.Scanner;
+
+public class NmberDivisibleby5 {
+
+	public static void main(String[] args) {
+		Scanner scanner=new Scanner(System.in);
+		System.out.println("Enter the number :");
+		int num=scanner.nextInt();
+		if(num%5==0) {
+			System.out.println(+num +" is divisible by 5");
+		}else {
+			System.out.println(+num +" is not divisible by 5");
+		}
+
+	}
+
+}
